b945fa06c9adb2e22c8a8923c9d4eed522b4f0b3
rvianapy <roviol@gmail.com>
2019-01-22 07:55:54 -0200

    Aula 02

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,33 @@
-/*  $ é um atalho para a função seletora do jQuery, que leva
-    o mesmo nome do framework.
-    Assim, poderia ter sido usado tanto jQuery(".frase")
-    quanto $(".frase")
-    $(".frase") seleciona no HTML a tag que possui classe
-    "frase" e através do metodo text() extrai o seu conteúdo
-    gravando-o na variável "frase".
-*/
+/*  '$' é um atalho para a função seletora do jQuery, que leva
+ *  o mesmo nome do framework.
+ *  Assim, poderia ter sido usado tanto jQuery(".frase")
+ *  quanto $(".frase")
+ *  $(".frase") seleciona no HTML a tag que possui classe
+ *  "frase" e através do metodo text() extrai o seu conteúdo
+ *  gravando-o na variável "frase".
+ */
 var frase = $(".frase").text();
 
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("click", function() {
+    
+    // Pega o texto digitado no campo e salva em 'conteudo'
+    var conteudo = campo.val();
+    /* Conta a quantidade de palavras digitadas
+     * por meio da função 'split()' usando como
+     * delimitador um espaço em branco 'split(" ")'
+     */ 
+    var qtdPalavras = conteudo.split(" ").length;
+    console.log(qtdPalavras);
+
+    /* Substitui a tag com id 'contador-palavras'
+     * com o conteúdo da variável 'qtdPalavras'
+     */
+    $("#contador-palavras").text(qtdPalavras);
+});
+
