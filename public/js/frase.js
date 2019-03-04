a910bb3c940102356dc0fd995cd47d58bee0e5af
rvianapy <roviol@gmail.com>
2019-03-04 00:07:27 -0300

    Eu bebi vinho e não estou em condições de descrever esse commit.

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,20 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases",trocaFraseAleatoria);   
+    
+    $("#spinner").toggle(); // mostrando o spinner
+
+    $.get("http://localhost:3000/frases",trocaFraseAleatoria)
+    .fail(function() {
+        $("#erro").toggle();
+        setTimeout(function() {
+            $("#erro").toggle();
+        },1500);
+    })
+    .always(function() {
+        $("#spinner").toggle();
+    });
 }
 
 function trocaFraseAleatoria(data) {
@@ -10,4 +23,31 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numAleatorio].tempo);
+}
+
+function buscaFrase() {
+    
+    $("#spinner").toggle();
+
+    var fraseId = $("#frase-id").val();
+    var dados = {id: fraseId}; // criacao do objeto JS que guarda a id
+
+    // passando objeto como segundo parametro
+    $.get("http://localhost:3000/frases",dados, trocaFrase)
+    .fail(function() {
+        $("#erro").toggle();
+        setTimeout(function() {
+            $("#erro").toggle();
+        },2000);
+    })
+    .always(function() {
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+    var frase = $(".frase");
+    frase.text(data.texto) // cuidado, texto com "o" no final
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
